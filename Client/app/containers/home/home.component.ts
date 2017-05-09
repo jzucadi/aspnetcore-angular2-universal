ae63b679e4bc3f74f4ffbdc0f6404a5563473446
James Zaccardo <jameszaccardo@gmail.com>
2017-05-09 15:50:52 -0700

    tryin

--- Client/app/containers/home/home.component.ts
+++ Client/app/containers/home/home.component.ts
@@ -8,7 +8,7 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class HomeComponent implements OnInit {
 
-    title: string = 'Angular 4.0 Universal & ASP.NET Core advanced starter-kit';
+    title: string = 'Smart Recs Base';
 
     // Use "constructor"s only for dependency injection
     constructor(public translate: TranslateService) { }
